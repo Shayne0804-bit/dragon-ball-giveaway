bc13b2963964894c874902d5148a29fc32f21caa
Shayne0804-bit <sossoukassim@gmail.com>
2026-01-19 07:30:10 +0100

    🔧 Corriger l'événement Discord à 'clientReady' (version actuelle)

--- server/services/discordBot.js
+++ server/services/discordBot.js
@@ -51,7 +51,7 @@ class DiscordBotService {
         ],
       });
 
-      this.client.once('ready', () => {
+      this.client.once('clientReady', () => {
         this.isReady = true;
         console.log(`[DISCORD] ✅ Bot connecté: ${this.client.user.tag}`);
       });
